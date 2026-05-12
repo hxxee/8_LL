2b9705e6731e4b0865d9fa3a7a3560fb037e86ce
hahee <hahee_hi@naver.com>
2026-05-13 00:48:28 +0900

    font

--- src/pages/GlobalStyles.jsx
+++ src/pages/GlobalStyles.jsx
@@ -1,6 +1,14 @@
 import { createGlobalStyle } from "styled-components";
 
-const GlobalStyle = createGlobalStyle`
+const GlobalStyle = createGlobalStyle`  
+  @font-face { font-family: 'Pretendard'; src: url('/fonts/pretendard/Pretendard-Regular.woff2') format('woff2'); font-weight: 400; font-display: swap; } 
+  
+  @font-face { font-family: 'Pretendard'; src: url('/fonts/pretendard/Pretendard-Medium.woff2') format('woff2'); font-weight: 500; font-display: swap; } 
+  
+  @font-face { font-family: 'Pretendard'; src: url('/fonts/pretendard/Pretendard-SemiBold.woff2') format('woff2'); font-weight: 600; font-display: swap; } 
+  
+  @font-face { font-family: 'Pretendard'; src: url('/fonts/pretendard/Pretendard-Bold.woff2') format('woff2'); font-weight: 700; font-display: swap; }
+  
   @font-face {
 	font-family: 'Pretendard Variable';
 	font-weight: normal;
